210bce6fc47c46952a8e491c050f54647209fea2
Marcelo Lisboa Cherem <marcelo.lisboacherem@gmail.com>
2025-03-11 12:58:30 +0100

    changing names and adding unity function - todo - add unity in products

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,19 +11,19 @@ function updateSpinnerColor(input) {
 }
 
 function incrementValue(button) {
-    const input = button.previousElementSibling; // Seleciona o campo antes do botão "+"
+    const input = button.previousElementSibling; // button "+"
     const currentValue = parseInt(input.value, 10) || 0;
-    input.value = currentValue + 1; // Incrementa o valor
-    updateSpinnerColor(input); // Atualiza a cor após incrementar
+    input.value = currentValue + 1; // valore +1
+    updateSpinnerColor(input); // cambia colore
 }
 
 function decrementValue(button) {
-    const input = button.nextElementSibling; // Seleciona o campo após o botão "-"
+    const input = button.nextElementSibling; // button "-"
     const currentValue = parseInt(input.value, 10) || 0;
     if (currentValue > 0) {
-        input.value = currentValue - 1; // Decrementa o valor sem ir abaixo de 0
+        input.value = currentValue - 1; // valore -1 non meno di 0
     }
-    updateSpinnerColor(input); // Atualiza a cor após decrementar
+    updateSpinnerColor(input); // cambia colore
 }
 
 
@@ -37,81 +37,86 @@ const productSuppliers = {
     "Tequila Espolon Promo": "Planet",
     "Bombolla": "Planet",
     "Caramello Salato": "Planet",
-    "lime": "Planet",
-    "melograno": "Planet",
-    "frutti di bosco": "Planet",
-    "cocco": "Planet",
-    "ananas": "Planet",
-    "axilu": "Stefano",
-    "endrizzi DOC": "Stefano",
-    "gewurztraminer": "Stefano",
-    "corona Cero": "Stefano",
-    "fusto Bud": "Stefano",
-    "coca Lattina": "Stefano",
-    "coca Zero Lattina": "Stefano",
-    "tè Pesca": "Stefano",
-    "tè Limone": "Stefano",
-    "limoncello": "Stefano",
-    "fanta": "Stefano",
+    "Lime": "Planet",
+    "ODK Melograno": "Planet",
+    "ODK Frutti di Bosco": "Planet",
+    "ODK Cocco": "Planet",
+    "ODK Ananas": "Planet",
+    "Axilu": "Stefano",
+    "Endrizzi DOC": "Stefano",
+    "Gewurztraminer": "Stefano",
+    "Corona Cero": "Stefano",
+    "Fusto Bud": "Stefano",
+    "Coca Lattina": "Stefano",
+    "Coca Zero Lattina": "Stefano",
+    "Tè Pesca": "Stefano",
+    "Tè Limone": "Stefano",
+    "Limoncello": "Stefano",
+    "Fanta": "Stefano",
     "Leffe": "Stefano",
-    "ginger beer": "Stefano",
-    "fusto coca": "Stefano",
-    "mirtilli": "metro",
-    "zucchero x caffè": "metro",
-    "cioccolatini": "metro",
-    "birra los amigos": "metro",
-    "ghiaccio cubbo": "metro",
-    "ghiaccio trit": "metro",
-    "succo mango": "metro",
-    "polpa mango": "metro",
-    "jalapenos": "metro",
-    "caffè": "metro",
-    "caffè deca": "metro",
-    "caffè ginseng": "metro",
-    "borghetti": "metro",
-    "alpino": "metro",
-    "acqua nat": "metro",
-    "acqua friz": "metro",
-    "tagin": "metro",
-    "lime": "metro",
-    "sale rosa": "metro",
-    "wood smoke": "metro",
-    "succo Arancia": "metro",
-    "succo melograno": "metro",
-    "succo ananas": "metro",
-    "schiroppo di agave": "metro",
-    "pompelmo soda": "metro",
-    "sangria": "metro",
-    "lugana": "metro",
-    "bepim": "metro",
-    "teroldego": "metro",
-    "tarapacà": "metro",
-    "lemonsoda": "metro",
-    "la señorita": "metro",
-    "el musico": "metro",
-    "modelo special": "metro",
-    "los amantes": "metro",
-    "sam migel": "metro",
-    "rooster rojo": "metro",
-    "don ramon plata": "metro",
-    "butterfly cannon cristalino": "metro",
-    "el jimador reposado": "metro",
-    "altos reposado": "metro",
-    "casamigos reposado": "metro",
-    "sierra antigo añejo": "metro",
-    "1800 añejo": "metro",
-    "rooster rojo añejo": "metro",
-    "ron prohibido": "metro",
-    "rom uruapan charanda": "metro",
-    "whisky pietro": "metro",
-    "san cosme": "metro",
-    "ojo de tigre blanco": "metro",
-    "bruxo": "metro",
-    "alipus san juan": "metro",
-    "alipus baltazar": "metro",
-    "monte alban": "metro",
-    "ojo de tigre reposado": "metro"
+    "Ginger Beer": "Stefano",
+    "Fusto Coca": "Stefano",
+    "Mirtilli": "metro",
+    "Zucchero x Caffè": "metro",
+    "Cioccolatini": "metro",
+    "Birra Los Amigos": "metro",
+    "Ghiaccio Cubo": "metro",
+    "Ghiaccio Trit": "metro",
+    "Succo Mango": "metro",
+    "Polpa Mango": "metro",
+    "Jalapenos": "metro",
+    "Caffè": "metro",
+    "Caffè Deca": "metro",
+    "Caffè Ginseng": "metro",
+    "Borghetti": "metro",
+    "Alpino": "metro",
+    "Acqua Nat": "metro",
+    "Acqua Friz": "metro",
+    "Tajin": "metro",
+    "ODK Lime": "metro",
+    "Sale Rosa": "metro",
+    "Wood Smoke": "metro",
+    "Succo Arancia": "metro",
+    "Succo Melograno": "metro",
+    "Succo Ananas": "metro",
+    "Schiroppo di Agave": "metro",
+    "Pompelmo Soda": "metro",
+    "Sangria": "metro",
+    "Lugana": "metro",
+    "Bepin": "metro",
+    "Teroldego": "metro",
+    "Tarapacà": "metro",
+    "Lemonsoda": "metro",
+    "La Señorita": "metro",
+    "El Musico": "metro",
+    "Modelo Special": "metro",
+    "Los Amantes": "metro",
+    "San Miguel": "metro",
+    "Rooster Rojo": "metro",
+    "Don Ramon Plata": "metro",
+    "Butterfly Cannon Cristalino": "metro",
+    "El Jimador Reposado": "metro",
+    "Altos Reposado": "metro",
+    "Casamigos Reposado": "metro",
+    "Sierra Antigo Añejo": "metro",
+    "1800 Añejo": "metro",
+    "Rooster Rojo Añejo": "metro",
+    "Ron Prohibido": "metro",
+    "Ron Uruapan Charanda": "metro",
+    "Whisky Prieto": "metro",
+    "San Cosme": "metro",
+    "Ojo de Tigre Blanco": "metro",
+    "Bruxo": "metro",
+    "Alipus San Juan": "metro",
+    "Alipus Baltazar": "metro",
+    "Monte Alban": "metro",
+    "Ojo de Tigre Reposado": "metro"
 };
+//unità
+const productUnits = {
+    //todo - unità di ogniuno prodotto
+};
+
 
 function toggleDropdown(id) {
     const dropdown = document.getElementById(id);
@@ -127,8 +132,10 @@ function saveSelection() {
     products.forEach(product => {
         const name = product.querySelector('span').innerText;
         const quantity = parseInt(product.querySelector('input').value, 10) || 0;
+        const unit = productUnits[name] || "unità";
+
         if (quantity > 0) {
-            result += `${name}, ${quantity}; \n`;
+            result += `${name}, ${quantity} ${unit}; \n`;
             hasSelectedProduct = true;
         }
     });
@@ -161,14 +168,16 @@ function generateMessages() {
 
         if (quantity && quantity > 0) {
             const supplier = productSuppliers[name]; // cerca fornitore
+            const unit = productUnits[name] || "unidade";
+
             if (supplier === "Planet") {
-                planetMessage += `${name}, ${quantity}; \n`;
+                planetMessage += `${name}, ${quantity} ${unit}; \n`;
                 planetHasProducts = true; // prodotti planet
             } else if (supplier === "Stefano") {
-                stefanoMessage += `${name}, ${quantity}; \n`;
+                stefanoMessage += `${name}, ${quantity} ${unit}; \n`;
                 stefanoHasProducts = true; // prodotti stefano
             } else if (supplier === "metro") {
-                metroMessage += `${name}, ${quantity}; \n`;
+                metroMessage += `${name}, ${quantity} ${unit}; \n`;
                 metroHasProducts = true; // prodotti metro
             }
         }
